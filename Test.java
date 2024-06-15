ea4ec1955cd0643c1b89f48197a38bc78b6e0b26
trygfmi <XXXXX@XXXXXX>
2024-06-15 22:21:17 +0900

    new comment

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class Test{
     public static void main(String args[]){
         System.out.println("hello world!");
         System.out.println("this is a new comment");
+        System.out.println("actions usage");
     }
 }
