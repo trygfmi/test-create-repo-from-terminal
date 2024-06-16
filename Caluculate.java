1011daaacbd5ada6c6e2ea3f77c72b57601d4ee8
trygfmi <XXXXX@XXXXXX>
2024-06-16 17:53:25 +0900

    add another java file

---
+++ Caluculate.java
@@ -0,0 +1,13 @@
+public class Caluculate {
+    int a;
+    int b;
+
+    Caluculate(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
