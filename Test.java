418e91a65540e6fe027d3ab8047260ccd574273d
trygfmi <XXXXX@XXXXXX>
2024-06-17 14:34:25 +0900

    modified HelloWorld.java Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,19 @@
-class Test{
+public class Test{
+
+    public int testpublicint;
+    int testvoidint;
+
     public static void main(String args[]){
         System.out.println("hello world!");
         System.out.println("this is a new comment");
-        System.out.println("actions usage");
+        System.out.println("actions usage!");
+    }
+
+    public void testpublicmethod(){
+
+    }
+
+    void testvoidmethod(){
+
     }
 }
